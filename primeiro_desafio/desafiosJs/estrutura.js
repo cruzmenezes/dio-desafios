f2922ebea56c196b76c27b79cdf2369d5641fade
Edson_Victor <cruz.menezes@outlook.com>
2023-04-29 18:46:34 -0300

    estruturas_new javascript

--- primeiro_desafio/desafiosJs/estrutura.js
+++ primeiro_desafio/desafiosJs/estrutura.js
@@ -8,12 +8,46 @@
 // CRIA UM PROGRAMA QUE SEJA CAPAZ DE PERCORRER UMA LISTA DE NUMERO E 
 // IMPRIMA CADA NUMERO DA LISTA.
 
-const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-for (let i = 0; i < numeros.length; i++) {
-    const numero = numeros[i];
-    if (numero % 2 === 0) {
-        console.log(numero);
-    }
+// for (let i = 0; i < numeros.length; i++) {
+//     const numero = numeros[i];
+//     if (numero % 2 === 0) {
+//         console.log(numero);
+//     }
 
-}
+// }
+
+// const numeros = [];
+
+// for (let i = 0; i < 10; i++) {
+//     numeros.push(i);
+// }
+
+// console.log(numeros);
+
+
+// const numerosPares = [];
+
+//     for (let i = 0; i < 10; i = i + 2) {
+//         numerosPares.push(i);
+//     }
+
+//     console.log(numerosPares);
+
+
+// const numerosPares = [];
+
+// for (let i = 0; i < 10; i++) {
+//     const numeroPar = i % 2 === 0;
+//     if (numeroPar) {
+//         numerosPares.push(i);
+//     }
+// }
+
+// console.log(numerosPares);
+
+
+const listaDeValores = [1, 2, 3, 4, 5];
+    const valor = listaDeValores[2];
+    console.log(valor);
