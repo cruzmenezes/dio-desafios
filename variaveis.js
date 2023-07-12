f945ed53492b1efddeafc96fd4b6699fdb3a5ea9
Edson Victor da cruz Menezes <91231848+cruzmenezes@users.noreply.github.com>
2023-07-12 12:26:06 +0000

    testando.....

---
+++ variaveis.js
@@ -0,0 +1,22 @@
+let nu1 = 10;
+let nu2 = 20;
+
+let soma = nu1 + nu2
+
+let tex = "texto"
+
+
+console.log (`A soma dos numeros é: ${soma}, \n e o texto é: ${tex}`);
+
+
+console.clear();
+
+
+function soma1(nu1, nu2){
+    return nu1 + nu2
+}
+
+console.log(soma1(10, 10));
+
+
+
